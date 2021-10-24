42382ef66b9472a78bca719df883930c4b5cdf03
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2021-10-24 23:08:12 +0900

    Add embed

--- commands/vc/settings.js
+++ commands/vc/settings.js
@@ -42,7 +42,12 @@ module.exports = class VCCommand extends Command {
     if (args[0] == "speaker") {
       const userSetting = await messageReader.guilds.get(message.guild)._getUserSetting(message.author.id);
       if (!args[1]) return message.channel.send(`指定できる声のリストは、こちらです。\n\`\`\`${allowedVoiceList}\`\`\`\n現在の声の設定は、${userSetting.speaker}です。`);
-      if (!allowedVoiceList.includes(args[1])) return message.channel.send(`その声(${args[1]})は指定できません。指定できる声のリストは、こちらです。\n\`\`\`${allowedVoiceList}\`\`\``)
+      if (!allowedVoiceList.includes(args[1])) return message.channel.send({
+        embed: {
+          title: "エラー",
+          description: `その声(${args[1]})は指定できません。指定できる声のリストは、こちらです。\n\`\`\`${allowedVoiceList}\`\`\``
+        }
+      })
       messageReader.guilds.get(message.guild)._setUserSetting(message.author.id, "speaker", args[1]);
       message.channel.send({
         embed: {
@@ -53,7 +58,12 @@ module.exports = class VCCommand extends Command {
     }
 
     if (args[0] == "pitch") {
-      if (!args[1]) return message.channel.send(`指定できる声の高さ、50%~200%です。\n現在の声の高さは、${userSetting.pitch}%です。`);
+      if (!args[1]) return message.channel.send(
+        {embed: {
+          color: 0xFF0000,
+          title: "エラー",
+          description: `指定できる声の高さ、50%~200%です。\n現在の声の高さは、${userSetting.pitch}%です。`
+        }}
       if (args[1] < 50 || args[1] > 200 || !args[1].match(/^[0-9]*$/g)) return message.channel.send(`その高さ(${args[1]}%)は指定できません。指定できる声の高さは、50%~200%です。`)
       messageReader.guilds.get(message.guild)._setUserSetting(message.author.id, "pitch", args[1]);
       message.channel.send({
@@ -67,7 +77,12 @@ module.exports = class VCCommand extends Command {
     if (args[0] == "speed") {
       const userSetting = await messageReader.guilds.get(message.guild)._getUserSetting(message.author.id);
       if (!args[1]) return message.channel.send(`指定できる声の速度は、50%~400%です。\n現在の声の速度は、${userSetting.speed}%です。`);
-      if (args[1] < 50 || args[1] > 400 || !args[1].match(/^[0-9]*$/g)) return message.channel.send(`その速度(${args[1]}%)は指定できません。指定できる声の速度は、50%~400%です。`)
+      if (args[1] < 50 || args[1] > 400 || !args[1].match(/^[0-9]*$/g)) return message.channel.send(
+        {embed: {
+          color: 0xFF0000,
+          title: "エラー",
+          description: `その速度(${args[1]}%)は指定できません。指定できる声の速度は、50%~400%です。`
+        }}
       messageReader.guilds.get(message.guild)._setUserSetting(message.author.id, "speed", args[1]);
       message.channel.send({
         embed: {

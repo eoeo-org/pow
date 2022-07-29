2e86fba7f1eb9dd23e8bdd1399c7be0199522b3e
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2022-07-29 12:08:51 +0900

    Minor update

--- commands/read.js
+++ commands/read.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders')
+const { SlashCommandBuilder } = require('discord.js')
 const { objToList } = require('../utils.js')
 const convertContent = require('../contentConverter')
 const voiceRead = require('../voiceRead')

6910419b316c0faabf50bebe5636ed69d8b2ee31
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2022-07-29 17:04:43 +0900

    Minor update (#107)

--- commands/join.js
+++ commands/join.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders')
+const { SlashCommandBuilder } = require('discord.js')
 const voiceRead = require('../voiceRead')
 
 module.exports = {
--- deployGlobalCommand.js
+++ deployGlobalCommand.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const fs = require('fs')
 const { REST } = require('@discordjs/rest')
-const { Routes } = require('discord-api-types/v9')
+const { Routes } = require('discord.js')
 const commands = []
 const commandFiles = fs
   .readdirSync('./commands')

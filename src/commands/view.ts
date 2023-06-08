982d2d24555eb5df16578ac61073694cbbb3c4ae
Mogyuchi <mogyuchi@mogyuchi.jp>
2023-06-08 21:22:15 +0900

    Update view command (#572)
    ユーザーメンションになるように変更

--- src/commands/view.ts
+++ src/commands/view.ts
@@ -38,16 +38,14 @@ export class ViewCommand extends Command {
       user ? user.id : interaction.member.id,
     )
 
-    const userNameText = user
-      ? `${user.username}#${user.discriminator}`
-      : '現在'
+    const userNameText = user ? user : '現在'
 
     return interaction.reply({
       embeds: [
         {
           color: 0x00ff00,
-          title: `${userNameText}の声の設定`,
           description:
+            `**${userNameText}の声の設定**` +
             '```\n' +
             objToList(userSetting).split('\n').slice(1).join('\n') +
             '\n```',

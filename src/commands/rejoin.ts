1aac74bf2b88110d16576a87fadbda3d4615b3a3
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2023-08-25 21:05:34 +0900

    feat(command): Add /rejoin command. (#715)

---
+++ src/commands/rejoin.ts
@@ -0,0 +1,95 @@
+import { Command, type ChatInputCommand } from '@sapphire/framework'
+import { guildCtxManager } from '../index.js'
+import type { Client } from 'discord.js'
+
+export class RejoinCommand extends Command {
+  public constructor(
+    context: ChatInputCommand.Context,
+    options: ChatInputCommand.Options,
+  ) {
+    super(context, {
+      ...options,
+      description: 'ボイスチャンネルに再接続します。',
+    })
+  }
+
+  public override registerApplicationCommands(
+    registry: ChatInputCommand.Registry,
+  ) {
+    registry.registerChatInputCommand((builder) =>
+      builder
+        .setName(this.name)
+        .setDescription(this.description)
+        .setDMPermission(false),
+    )
+  }
+  public override async chatInputRun(
+    interaction: ChatInputCommand.Interaction,
+  ) {
+    if (!interaction.inCachedGuild()) return
+    const user = await interaction.member.fetch()
+    const voiceChannel = user.voice.channel
+    if (voiceChannel == null) {
+      return interaction.reply({
+        embeds: [
+          {
+            color: 0xff0000,
+            title: 'エラー',
+            description:
+              'このコマンドを実行するには、VCに参加している必要があります。',
+          },
+        ],
+        ephemeral: true,
+      })
+    }
+
+    const guildCtx = guildCtxManager.get(interaction.member.guild)
+
+    if (!guildCtx.connectionManager.channelMap.has(voiceChannel)) {
+      return interaction.reply({
+        embeds: [
+          {
+            color: 0xff0000,
+            title: 'エラー',
+            description: 'BOTと同じVCに参加している必要があります。',
+          },
+        ],
+        ephemeral: true,
+      })
+    }
+
+    const textChannel = guildCtx.connectionManager.channelMap.get(voiceChannel)
+
+    guildCtx.leave(voiceChannel)
+
+    let worker: Client | null = null
+
+    try {
+      worker = await guildCtx.join(voiceChannel, interaction.channel!)
+    } catch (err) {
+      if (err instanceof Error && err.message === 'No worker') {
+        return interaction.reply({
+          embeds: [
+            {
+              color: 0xff0000,
+              title: 'エラー',
+              description: '参加させられるBotが居ません。',
+            },
+          ],
+          ephemeral: true,
+        })
+      }
+      throw err
+    }
+
+    return interaction.reply({
+      embeds: [
+        {
+          color: 0x00ff00,
+          title: 'ボイスチャンネルに再接続しました。',
+          description: `担当BOT: <@${worker.user?.toString()}>\nテキストチャンネル: ${textChannel}\nボイスチャンネル: ${voiceChannel}`,
+        },
+      ],
+    })
+  }
+}

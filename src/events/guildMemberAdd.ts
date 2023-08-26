9267ba97cd2f5e63d04fcea561c4ad17b0df0692
Mogyuchi <mogyuchi@mogyuchi.jp>
2023-08-26 11:48:38 +0900

    refactor: events を Sapphire 管理下に移動 (#726)

--- src/events/guildMemberAdd.ts
+++
@@ -1,8 +0,0 @@
-import type { GuildMember } from 'discord.js'
-import { guildCtxManager, workerClientMap } from '../index.js'
-
-export const guildMemberAddEvent = async (member: GuildMember) => {
-  if (workerClientMap.has(member.id)) {
-    await guildCtxManager.get(member.guild).addBot(member.id)
-  }
-}
--- src/events/index.ts
+++ src/events/index.ts
@@ -1,6 +1,1 @@
-export { channelUpdateEvent } from './channelUpdate.js'
-export { guildMemberAddEvent } from './guildMemberAdd.js'
-export { guildMemberRemoveEvent } from './guildMemberRemove.js'
-export { messageCreateEvent } from './messageCreate.js'
 export { readyEvent } from './ready.js'
-export { voiceStateUpdateEvent } from './voiceStateUpdate.js'

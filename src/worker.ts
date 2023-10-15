2931409449d079531e3c6b1544409415894f9797
Mogyuchi <mogyuchi@mogyuchi.jp>
2023-10-15 17:51:34 +0900

    feat: 「プレイ中」表記がない presence にする

--- src/worker.ts
+++ src/worker.ts
@@ -1,4 +1,4 @@
-import { Client, Events } from 'discord.js'
+import { ActivityType, Client, Events } from 'discord.js'
 
 export class WorkerNullError extends Error {
   code = 'WORKER_USER_IS_NULL' as const
@@ -12,7 +12,12 @@ class WorkerClient<Ready extends boolean = boolean> extends Client<Ready> {
   #onReady(client: Client<true>) {
     console.log(`Ready as ${client.user.tag}`)
     client.user.setPresence({
-      activities: [{ name: `${this.mainClient.user.tag} - worker` }],
+      activities: [
+        {
+          name: `${this.mainClient.user.tag} - worker`,
+          type: ActivityType.Custom,
+        },
+      ],
       status: 'dnd',
     })
   }

168e41404b3ca8e19d285a42e00b7825f8a2e66e
Mogyuchi <mogyuchi@mogyuchi.jp>
2024-03-30 15:40:59 +0900

    chore: configure ESLint

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: ['cli-incremental-info', 'coverage', 'dist/*'],
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      ecmaVersion: 2023,
+      globals: {
+        ...globals.node,
+      },
+      parserOptions: {
+        allowAutomaticSingleRunInference: true,
+        cacheLifetime: {
+          glob: 'Infinity',
+        },
+        project: ['tsconfig.json', 'tsconfig.*.json'],
+      },
+    },
+  },
+)
